3d955ac6bd033d7295d801d3e24b6a2a2b421856
21hyin <51760615+21hyin@users.noreply.github.com>
2020-01-20 20:06:45 -0500

    21hyin patch 1 (#16)
    * Tie loose ends
    
    Set normalized value of a feature to 0.5 when they all have the same value
    Modified script to loop through csv files in a directory
    
    * Update normalization_v2.py
    
    * Update feat_vec_csv_builder.py
    
    * Create see_data.py

--- feat_vec_csv_builder.py
+++ feat_vec_csv_builder.py
@@ -1,24 +1,25 @@
+import os
 import numpy as np
 import pandas as pd
-import os
 import cv2
-
-# get list of all image directories
-img_dir =  list(filter(lambda x: x.endswith('images'), [x[0] for x in os.walk('.')]))
-for x in img_dir:
-    img_dir_to_csv(x)
 
 def img_dir_to_csv(path):
     '''creates a feature vector csv from a directory of images'''
-    csv_file = path + '_fv.csv'
+    # csv_file = path + '_fv.csv'
     print('creating feature vector csv for ', path)
     images = os.listdir(path)
     img_fv = np.array(list(map(lambda x: img_to_feat_vec(path + '/' + x), images)))
-    pd.DataFrame(data=img_fv, index=images, columns=img_fv[0]).to_csv(csv_file)
+    (pd.DataFrame(data = img_fv, index = images)).to_csv(path + '_fv.csv', header = False)
 
 def img_to_feat_vec(img_name):
     '''converts an image to a feature vector'''
-    img = cv2.imread(img_name)
+    img = cv2.imread(img_name) # STORED Blue | Green | Red
     (means, stds) = cv2.meanStdDev(img)
-    stats = np.concatenate([means, stds]).flatten()
+    stats = np.concatenate([means, stds, np.square(stds)]).flatten()
     return stats
+
+# get list of all image directories
+img_dir = list(filter(lambda x: x.endswith('images'), [x[0] for x in os.walk('.')]))
+
+for x in img_dir:
+    img_dir_to_csv(x)
