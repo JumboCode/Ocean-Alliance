049ff2e619226f36f0ee6c28d7de76fcf498b297
mikaylaclark <mikayla.clark@tufts.edu>
2020-02-24 00:34:24 +0000

    Adding gallery_view.js

--- views/scripts/gallery_view.js
+++ views/scripts/gallery_view.js
@@ -1,14 +1,23 @@
+const $ = require('jquery')
 var inputFiles = [];
 inputFiles[0] = "I am  string";
 inputFiles[1] = "I am also a string";
 
 window.onload = function () {
     for (var i = 0; i < inputFiles.length; i++) {
-	var row = document.createElement("p");
+	var string = "div";
+        var num = i.toString();
+        var row = document.createElement("div");
+        row.setAttribute("id", string.concat(num));
 	var text = document.createTextNode(inputFiles[i]);
 	row.appendChild(text);
 
 	var element = document.getElementById("row");
-	element.appendChild(row);
+	document.body.insertBefore(row, element);
     }
 }
+$(document).ready(function() {
+    $ ( "body" ).click(function( event ) {
+      $( "#squares" ).html( "clicked: " + event.target.nodeName );
+    });
+});
